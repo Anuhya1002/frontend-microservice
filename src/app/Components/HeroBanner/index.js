f0316d6eb0835ddb30468f1a5a3ee6860db63be6
Anuhya1002 <anuhyagangireddy@gmail.com>
2024-11-25 19:35:54 +0530

    new changes new push

--- src/app/Components/HeroBanner/index.js
+++ src/app/Components/HeroBanner/index.js
@@ -5,9 +5,6 @@ export default function HeroBanner() {
   return (
     <section className="container-fluid herobanner">
       <div className="d-flex flex-column align-items-center justify-content-center">
-        <div className="">
-          <button className="hello-btn">Hello</button>
-        </div>
         <h1 className="main-banner-text">I&apos;m Pavan Gangireddy,</h1>
         <Typewritertext />
       </div>
